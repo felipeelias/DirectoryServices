f4313bc36f10a6f4e273fc29b5c261f67765ff35
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-02 17:59:04 +0100

    add register/unregister methods

--- DirectoryServices.rb
+++ DirectoryServices.rb
@@ -2,4 +2,12 @@ class DirectoryServices
   def self.registered_hosts
     `/usr/bin/dscl localhost -list /Local/Default/Hosts`.split("\n")
   end
+  
+  def self.register(host)
+    system("/usr/bin/dscl localhost -create /Local/Default/Hosts/#{host} IPAddress 127.0.0.1")
+  end
+  
+  def self.unregister(host)
+    system("/usr/bin/dscl localhost -delete /Local/Default/Hosts/#{host}")
+  end
 end
