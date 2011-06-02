de82635d971806f45ec818bf12a75faf3b47dd11
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-02 14:59:47 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DirectoryServices
+//
+//  Created by Felipe Elias Philipp on 02/06/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
